3b553fc3dc31cc9ec41556cf324f28bfaaecc2bb
oscmag <oscarbmagnusson@gmail.com>
2019-03-27 00:15:11 +0100

    basic setup

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {MainProvider} from './context/MainContext';
+import {Router, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Sudoku from './components/Sudoku';
+import Workouts from './components/Workouts.js'
+import Statistics from './components/Statistics.js'
+import About from './components/About.js'
+import './App.css';
+import history from './history';
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={history}>
+        <div className="App">
+          <MainProvider>
+            <Navbar />
+            <div className="app-body">
+              <Route exact path="/" component={Sudoku}/>
+              <Route exact path="/Sudoku" component={Sudoku}/>
+              {/* <Route exact path="/workouts" component={Workouts} />
+              <Route exact path="/statistics" component={Statistics} />
+              <Route exact path="/about" component={About} /> */}
+            </div>
+          </MainProvider>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+class About extends React.Component {
+
+  render() {
+    return (
+      <div className="about-page">
+        <p>This is Getting There. A page where you can keep track of your workouts with extensive statistics.</p>
+      </div>
+    );
+  }
+}
+
+export default About;
